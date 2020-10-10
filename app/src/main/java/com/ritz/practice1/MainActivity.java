458d2f39250c7f809efb5bc2905b9ed32c0649ce
Ritika Sagar <ritikasagar23@gmail.com>
2020-10-10 15:07:02 +0800

    first commit

---
+++ app/src/main/java/com/ritz/practice1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.ritz.practice1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText enterItem;
+    Button save;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enterItem = findViewById(R.id.enter_item);
+        save = findViewById(R.id.save);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = enterItem.getText().toString();
+                if(item.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Please enter text first", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
